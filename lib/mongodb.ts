ce238a876848caed36ef208714d2482d7f4b67b5
Obire John <youremail@gmail.com>
2026-04-01 06:17:14 -0700

    fix mongodb deploy issue

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,17 +1,17 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
-
-
-if (!MONGODB_URI) {
-  throw new Error("❌ MONGODB_URI is not defined");
-}
-
 export const connectDB = async () => {
   try {
+    const MONGODB_URI = process.env.MONGODB_URI;
+
+    if (!MONGODB_URI) {
+      throw new Error("❌ MONGODB_URI is not defined");
+    }
+
     if (mongoose.connection.readyState === 1) return;
 
     await mongoose.connect(MONGODB_URI);
+
     console.log("✅ MongoDB Connected");
   } catch (error) {
     console.log("MongoDB Error:", error);
