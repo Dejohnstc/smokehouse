4be8f6ecf076cb0e541087cfe2e16455692bc7d8
Obire John <youremail@gmail.com>
2026-04-01 05:51:15 -0700

    ready for production

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,16 +1,19 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI!;
+const MONGODB_URI = process.env.MONGODB_URI;
+
 
 if (!MONGODB_URI) {
-  throw new Error("Please define MONGODB_URI in .env.local");
+  throw new Error("❌ MONGODB_URI is not defined");
 }
 
 export const connectDB = async () => {
   try {
+    if (mongoose.connection.readyState === 1) return;
+
     await mongoose.connect(MONGODB_URI);
-    console.log("MongoDB Connected");
+    console.log("✅ MongoDB Connected");
   } catch (error) {
-    console.log(error);
+    console.log("MongoDB Error:", error);
   }
 };
